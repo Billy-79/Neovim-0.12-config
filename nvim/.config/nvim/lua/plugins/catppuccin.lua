119429f8e49c90e8a52e69c7745e23709a185f30
“Billy” <vasilisp@gmail.com>
2026-05-14 21:09:15 +0300

    Catppuccin Mocha colorscheme configutation

---
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+        auto_integrations = true,
+      })
+      vim.cmd.colorscheme "catppuccin-mocha"
+    end,
+  },
+}
